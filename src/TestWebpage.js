7ace752759b8f0b289c913a74d6cce3bac734ef8
LunarSamurai <josephcraig757@gmail.com>
2023-07-05 17:52:50 -0400

    Web Application Frontend 07/05/2023 5:53PM     -- Joseph Craig
        - Fixed issue where test files were not being received from the backend
        - Display text file line by line by the next button command
        - Trimmed the question that gets displayed
        - Fixed issues on the backend that stopped this functionality from working
    
    -- Joseph Craig
    
    Changes to be committed:
    	modified:   src/Test.css
    	modified:   src/TestWebpage.js

--- src/TestWebpage.js
+++ src/TestWebpage.js
@@ -4,7 +4,10 @@ import './Test.css'; // Import the CSS file
 function TestPage() {
   const [sortedTestOrders, setSortedTestOrders] = useState([]);
   const [currentTestIndex, setCurrentTestIndex] = useState(0);
-  const [currentTestFile, setCurrentTestFile] = useState(null);
+  const [currentTestLines, setCurrentTestLines] = useState([]);
+  const [currentLineIndex, setCurrentLineIndex] = useState(0);
+  const [lastLinesList, setLastLinesList] = useState([]);
+
 
   useEffect(() => {
     // Fetch the test orders
@@ -15,10 +18,15 @@ function TestPage() {
         const testOrdersArray = Array.isArray(data) ? data : [];
 
         // Sort the entries by test_order_number
-        const sortedOrders = testOrdersArray.sort((a, b) => a.test_order_number - b.test_order_number);
-
+        const sortedOrders = testOrdersArray.sort((a, b) => a.testOrderNumber - b.testOrderNumber);
+        console.log(sortedOrders);
         // Set the sorted test orders
         setSortedTestOrders(sortedOrders);
+
+        // Fetch and display the first test file initially
+        if (sortedOrders.length > 0) {
+          fetchTestFile(sortedOrders[0].textFileName);
+        }
       })
       .catch((error) => {
         console.error(error);
@@ -27,51 +35,85 @@ function TestPage() {
       });
   }, []);
 
-  useEffect(() => {
-    console.log('sortedTestOrders:', sortedTestOrders);
-    if (sortedTestOrders.length > 0) {
-      // Get the current test order
-      const currentTestOrder = sortedTestOrders[currentTestIndex];
+  const fetchTestFile = (textFileName) => {
+    fetch(`http://localhost:8080/api/get-test-file?fileName=${textFileName}`)
+      .then((response) => response.text())
+      .then((text) => {
+        const lines = text.split('\n');
+        const lastLine = lines.pop();
+        setCurrentTestLines(lines);
+        setCurrentLineIndex(0);
+        if (lastLine) {
+          setLastLinesList((prevList) => [...prevList, { fileName: textFileName, lastLine }]);
+          console.log(lastLinesList);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle any errors that occurred during the request
+        // Display an error message or perform appropriate error handling
+      });
+  };
 
-      // Fetch the test file based on the text_file_name
-      fetch(`http://localhost:8080/api/get-test-file?fileName=${currentTestOrder.text_file_name}`)
-        .then((response) => response.text())
-        .then((text) => {
-          // Set the current test file
-          setCurrentTestFile(text);
-        })
-        .catch((error) => {
-          console.error(error);
-          // Handle any errors that occurred during the request
-          // Display an error message or perform appropriate error handling
-        });
-    }
-  }, [sortedTestOrders, currentTestIndex]);
+  //Buttons For Each Question
+  const handleStronglyAgreeClick = () => {
 
+  }
+  const handleAgreeClick = () => {
+    
+  }
+  const handleNeitherClick = () => {
+    
+  }
+  const handleDisagreeClick = () => {
+    
+  }
+  const handleStronglyDisagreeClick = () => {
+    
+  }
   const handleNextButtonClick = () => {
-    // Check if there are more test files to display
-    if (currentTestIndex < sortedTestOrders.length - 1) {
-      // Increment the current test index
-      setCurrentTestIndex((prevIndex) => prevIndex + 1);
+    // Check if there are more lines in the current file
+    if (currentLineIndex < currentTestLines.length - 1) {
+      // Increment the current line index
+      const nextLineIndex = currentLineIndex + 1;
+      setCurrentLineIndex(nextLineIndex);
     } else {
-      // Handle case when all test files have been displayed
-      console.log('All test files have been displayed.');
+      // Check if there are more test files to display
+      if (currentTestIndex < sortedTestOrders.length - 1) {
+        // Increment the current test index
+        const nextIndex = currentTestIndex + 1;
+        setCurrentTestIndex(nextIndex);
+
+        // Fetch and display the next test file
+        const nextTestOrder = sortedTestOrders[nextIndex];
+        fetchTestFile(nextTestOrder.textFileName);
+      } else {
+        // Handle case when all test files have been displayed
+        console.log('All test files have been displayed.');
+      }
     }
   };
 
-  console.log('currentTestFile:', currentTestFile);
-
   return (
     <div>
-      <h1 className="TestWebpageTitle">Tests</h1>
-      {currentTestFile && (
-        <div>
-          <pre>{currentTestFile}</pre>
-          <button onClick={handleNextButtonClick}>Next</button>
+      {currentTestLines.length > 0 && (
+        <div className="TestQuestionContainer">
+          <p className="DisplayText">{currentTestLines[currentLineIndex].split(/[|]/)[0]}</p>
+          <div className="spacer">
+          </div>
+          <div className="choice-container">
+            <button className="button-strongly-agree" onClick={handleStronglyAgreeClick}>Strongly Agree</button>
+            <button className="button-agree" onClick={handleAgreeClick}>Agree</button>
+            <button className="button-neither" onClick={handleNeitherClick}>Neither</button>
+            <button className="button-disagree" onClick={handleDisagreeClick}>Disagree</button>
+            <button className="button-strongly-disagree" onClick={handleStronglyDisagreeClick}>Strongly Disagree</button>
+          </div>
+          <button className="button-next" onClick={handleNextButtonClick}>Next</button>
         </div>
       )}
     </div>
   );
+  
 }
 
 export default TestPage;
