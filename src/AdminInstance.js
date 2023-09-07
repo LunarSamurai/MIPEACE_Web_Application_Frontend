3eee9b647dded5d3d6e3d98fb1e3ca1fd5244d02
LunarSamurai <josephcraig757@gmail.com>
2023-09-07 17:52:11 -0400

    MIPEACE Web Application Frontend 09/07/2023 @17:49PM     -- Joseph Craig
        -- Complete Overall of All Systems
        -- Admin controls have been removed for now
        -- New landing page for seamless movement
        -- Added new hiearchial design to simplify and modularize code
        -- Made an easier platform to add new features
        -- Implemented entire program into the Testing Option in main menu
        -- Implemented a new navigation panel and admin screen for later implementation
        -- Biggest update so far
        -- Changes are so drastic that, version name has been changed to Version 2.0
    
    -- Joseph Craig
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   public/index.html
    	new file:   src/AdminInstance.js
    	new file:   src/AdminInstance.scss
    	modified:   src/App.css
    	modified:   src/App.js
    	new file:   src/MIPEACE_LOGO.png
    	new file:   src/TestInstance copy.js
    	new file:   src/TestInstance.css
    	modified:   src/index.css

---
+++ src/AdminInstance.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import TestInstance from './TestInstance.js';
+import App from './App.js';
+import './AdminInstance.scss';  // Importing the styles
+import MIPEACE_Logo from './MIPEACE_LOGO.png'
+import { useHistory } from 'react-router-dom';
+import { redirect } from 'react-router';
+
+function AdminInstance() {
+
+    const [activeItem, setActiveItem] = useState('Connections'); // Initial active item set to 'Connections'
+    const [redirectToRootPage, setRedirectToRootPage] = useState(false);
+    
+    const handleLogoutClick = () => {
+        console.log("Logout was clicked!");
+        localStorage.setItem('adminLoggedOut', 'true'); // Set a specific flag for admin logout
+        setRedirectToRootPage(true);
+        window.location.reload();
+    };
+
+    const AdminControlPanel = () => {
+        // Check specifically for admin logout
+        if (localStorage.getItem('adminLoggedOut') === "true") {
+            return <Redirect to="/" />
+        }
+        return (
+            <div className="admin-control-panel-wrapper">
+                <nav className="sidebar-navigation">
+                    <div className="menu-items">
+                        <ul>
+                            <li className={activeItem === 'Connections' ? 'active' : ''} onClick={() => setActiveItem('Connections')}>
+                                <i className="fa fa-share-alt"></i>
+                                <span className="tooltip">Examinee List</span>
+                            </li>
+                            <li className={activeItem === 'Devices' ? 'active' : ''} onClick={() => setActiveItem('Devices')}>
+                                <i className="fa fa-hdd-o"></i>
+                                <span className="tooltip">Results</span>
+                            </li>
+                            <li className={activeItem === 'Contacts' ? 'active' : ''} onClick={() => setActiveItem('Contacts')}>
+                                <i className="fa fa-newspaper-o"></i>
+                                <span className="tooltip">Manage Exams</span>
+                            </li>
+                            <li className={activeItem === 'Fax' ? 'active' : ''} onClick={() => setActiveItem('Fax')}>
+                                <i className="fa fa-print"></i>
+                                <span className="tooltip">Pull Reports</span>
+                            </li>
+                            <li className={activeItem === 'Settings' ? 'active' : ''} onClick={() => setActiveItem('Settings')}>
+                                <i className="fa fa-sliders"></i>
+                                <span className="tooltip">Settings</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <li className="logout-item" onClick={handleLogoutClick}>
+                        <i className="fa fa-sign-out"></i>
+                    <span className="tooltip">Logout</span>
+                </li>
+                </nav>
+            </div>
+        );
+    }
+
+    return (
+        <div className="AdminInstance">
+            <Router>
+                <Switch>
+                    <Route exact path="/adminInstance">
+                        <AdminControlPanel />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
+}
+    
+export default AdminInstance;
